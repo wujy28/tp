729d15a081e375dea47175a140fff90eb791dc6a
Long Nguyen Tan <longnguyentan2002@gmail.com>
2023-10-27 01:50:06 +0800

    Add JSON serialization for Record
    Implement JsonAdaptedRecord to enable JSON serialization for patient records.
    Update JsonSerializableAddressBook to work with new serialization.
    Enhance AddressBook model to support new serialization functionality.
    Introduce UniqueRecordList for ensuring record uniqueness.

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -7,7 +7,9 @@ import java.util.List;
 import javafx.collections.ObservableList;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.model.patient.Patient;
+import seedu.address.model.patient.Record;
 import seedu.address.model.patient.UniquePatientList;
+import seedu.address.model.patient.UniqueRecordList;
 
 /**
  * Wraps all data at the address-book level
@@ -16,6 +18,7 @@ import seedu.address.model.patient.UniquePatientList;
 public class AddressBook implements ReadOnlyAddressBook {
 
     private final UniquePatientList patients;
+    private final UniqueRecordList records;
 
     /*
      * The 'unusual' code block below is a non-static initialization block, sometimes used to avoid duplication
@@ -26,6 +29,7 @@ public class AddressBook implements ReadOnlyAddressBook {
      */
     {
         patients = new UniquePatientList();
+        records = new UniqueRecordList();
     }
 
     public AddressBook() {
@@ -50,12 +54,21 @@ public class AddressBook implements ReadOnlyAddressBook {
     }
 
     /**
+     * Replaces the contents of the record list with {@code records}.
+     * {@code records} must not contain duplicate records.
+     */
+    public void setRecords(List<Record> records) {
+        this.records.setRecords(records);
+    }
+
+    /**
      * Resets the existing data of this {@code AddressBook} with {@code newData}.
      */
     public void resetData(ReadOnlyAddressBook newData) {
         requireNonNull(newData);
 
         setPatients(newData.getPatientList());
+        setRecords(newData.getRecordList());
     }
 
     //// patient-level operations
@@ -100,12 +113,17 @@ public class AddressBook implements ReadOnlyAddressBook {
 
     @Override
     public String toString() {
-        return new ToStringBuilder(this).add("patients", patients).toString();
+        return new ToStringBuilder(this).add("patients", patients).add("records", records).toString();
     }
 
     @Override
     public ObservableList<Patient> getPatientList() {
         return patients.asUnmodifiableObservableList();
+    }
+
+    @Override
+    public ObservableList<Record> getRecordList() {
+        return records.asUnmodifiableObservableList();
     }
 
     @Override
@@ -120,11 +138,36 @@ public class AddressBook implements ReadOnlyAddressBook {
         }
 
         AddressBook otherAddressBook = (AddressBook) other;
-        return patients.equals(otherAddressBook.patients);
+        return patients.equals(otherAddressBook.patients) && records.equals(otherAddressBook.records);
     }
 
     @Override
     public int hashCode() {
         return patients.hashCode();
     }
+
+    //// record-level operations
+
+    /**
+     * Returns true if a record is the same as an existed {@code record} in the address book.
+     */
+    public boolean hasRecord(Record record) {
+        requireNonNull(record);
+        return records.contains(record);
+    }
+
+    public void addRecord(Record r) {
+        records.add(r);
+    }
+
+    public void setRecord(Record target, Record editedRecord) {
+        requireNonNull(editedRecord);
+        records.setRecord(target, editedRecord);
+    }
+
+    public void removeRecord(Record key) {
+        records.remove(key);
+    }
+
+
 }
